b26665dadf42c4dcc25e1396b172e49028061fd1
Svetlana Starikova <sveta.semochkina14@yandex.com>
2025-03-10 17:08:36 +0600

    update lion without dependencies

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -2,21 +2,22 @@ package com.example;
 
 import java.util.List;
 
+/*Класс Lion не должен зависеть от класса Feline. Используй принцип инъекции зависимостей.*/
 public class Lion {
 
-    boolean hasMane;
+    private final boolean hasMane;
+    private final Feline feline;
 
-    public Lion(String sex) throws Exception {
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
+        this.feline = feline;
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
@@ -27,6 +28,6 @@ public class Lion {
     }
 
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return feline.eatMeat(); // используем напрямую из Feline
     }
 }
